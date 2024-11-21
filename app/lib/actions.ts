b9c93883953f46b7733e74951389089b239600cd
Admin <mmeena@divinisoft.com>
2024-11-21 22:55:07 +0530

    Fixed lint issues

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -77,6 +77,8 @@ export async function createInvoice(prevState: State, formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
     } catch (error) {
+      console.log(error);
+      
         // If a database error occurs, return a more specific error.
         return {
             message: 'Database Error: Failed to Create Invoice.',
@@ -118,6 +120,7 @@ export async function updateInvoice(
         WHERE id = ${id}
       `;
     } catch (error) {
+      console.log(error);
       return { message: 'Database Error: Failed to Update Invoice.' };
     }
    
@@ -131,6 +134,7 @@ export async function deleteInvoice(id: string) {
         revalidatePath('/dashboard/invoices');
         return { message: 'Deleted Invoice.' };
     } catch (error) {
+      console.log(error);
         return { message: 'Database Error: Failed to Delete Invoice.' };
     }
 }
